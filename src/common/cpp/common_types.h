f1da21d579f77b180975eb5595a7d15d1478465b
sywu <w583957236@gmail.com>
2026-05-18 20:38:11 +0800

    refactor(common): add CMSharedPtr/CMUniquePtr/CMWeakPtr aliases and CMMakeShared/CMMakeUnique/CMPointerCast wrappers
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- src/common/cpp/common_types.h
+++ src/common/cpp/common_types.h
@@ -10,6 +10,8 @@
 #include <queue>
 #include <stack>
 #include <string>
+#include <memory>
+#include <utility>
 
 namespace fly {
 
@@ -42,8 +44,47 @@ using CMStack = std::stack<T>;
 
 using CMString = std::string;
 
+template<typename T>
+using CMSharedPtr = std::shared_ptr<T>;
+
+template<typename T>
+using CMUniquePtr = std::unique_ptr<T>;
+
+template<typename T>
+using CMWeakPtr = std::weak_ptr<T>;
+
 template<typename K, typename V>
 using CMMapKV = std::pair<K, V>;
+
+template<typename T, typename... Args>
+constexpr CMSharedPtr<T> CMMakeShared(Args&&... args) {
+    return std::make_shared<T>(std::forward<Args>(args)...);
+}
+
+template<typename T, typename... Args>
+constexpr CMUniquePtr<T> CMMakeUnique(Args&&... args) {
+    return std::make_unique<T>(std::forward<Args>(args)...);
+}
+
+template<typename T, typename U>
+constexpr CMSharedPtr<T> CMStaticPointerCast(const CMSharedPtr<U>& r) noexcept {
+    return std::static_pointer_cast<T>(r);
+}
+
+template<typename T, typename U>
+constexpr CMSharedPtr<T> CMDynamicPointerCast(const CMSharedPtr<U>& r) noexcept {
+    return std::dynamic_pointer_cast<T>(r);
+}
+
+template<typename T, typename U>
+constexpr CMSharedPtr<T> CMConstPointerCast(const CMSharedPtr<U>& r) noexcept {
+    return std::const_pointer_cast<T>(r);
+}
+
+template<typename T, typename U>
+constexpr CMSharedPtr<T> CMReinterpretPointerCast(const CMSharedPtr<U>& r) noexcept {
+    return std::reinterpret_pointer_cast<T>(r);
+}
 
 }
 
@@ -57,4 +98,13 @@ using fly::CMDeque;
 using fly::CMQueue;
 using fly::CMStack;
 using fly::CMString;
+using fly::CMSharedPtr;
+using fly::CMUniquePtr;
+using fly::CMWeakPtr;
 using fly::CMMapKV;
+using fly::CMMakeShared;
+using fly::CMMakeUnique;
+using fly::CMStaticPointerCast;
+using fly::CMDynamicPointerCast;
+using fly::CMConstPointerCast;
+using fly::CMReinterpretPointerCast;
